b8caec06ae3e0fc244e78684051b23f4b8dfda48
Jarupat <jarupat.cho@student.mahidol.edu>
2025-05-07 17:16:32 +0700

    try google api

--- src/api/index.js
+++ src/api/index.js
@@ -27,12 +27,103 @@ export const getPlacesData = async (type, sw, ne) => {
                 },
             }
         );
+        console.log(data)
 
         return data;
     } catch (error) {
         console.log(error);
     }
 };
+
+// export const getPlacesData = async (sw, ne) => {
+//     // Restrict within the map viewport.
+//     const { Place, SearchNearbyRankPreference } =
+//         await google.maps.importLibrary("places");
+
+//     const centerLat = (sw.lat + ne.lat) / 2;
+//     const centerLng = (sw.lng + ne.lng) / 2;
+
+//     let center = new google.maps.LatLng(centerLat, centerLng);
+//     const request = {
+//         // required parameters
+//         fields: [
+//             "displayName",
+//             "location",
+//             "businessStatus",
+//             "rating",
+//             "types",
+//             "websiteURI",
+//             "nationalPhoneNumber",
+//             "photos",
+//             "priceLevel",
+//             "priceRange",
+//             "primaryType",
+//             "userRatingCount",
+//             "primaryTypeDisplayName"
+//         ],
+//         locationRestriction: {
+//             center: center,
+//             radius: 1500,
+//         },
+//         includedPrimaryTypes: ["restaurant"],
+//         maxResultCount: 20,
+//         // rankPreference: SearchNearbyRankPreference.POPULARITY,
+//         language: "en-US",
+//         region: "us",
+//     };
+//     const { places } = await Place.searchNearby(request);
+
+//     console.log(places);
+
+//     const formatted = await Promise.all(
+//         places.map(async (place) => {
+//             // const photoUrls = await Promise.all(
+//             //     (place.photos || []).map(async (photo) => {
+//             //         const res = await fetch(
+//             //             `https://places.googleapis.com/v1/${photo.name}/media?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&maxWidthPx=1980&maxHeightPx=1080`
+//             //         );
+//             //         return res.url;
+//             //     })
+//             // );
+//             let photoUrl = null;
+
+//             if (place.photos && place.photos.length > 0) {
+//                 const firstPhotoName = place.photos[0].name;
+//                 const mediaUrl =
+//                     `https://places.googleapis.com/v1/${firstPhotoName}/media` +
+//                     `?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&maxWidthPx=1980&maxHeightPx=1080`;
+//                 const res = await fetch(mediaUrl, {
+//                     method: "GET",
+//                     redirect: "follow",
+//                 });
+//                 photoUrl = res.url; // this is the real image URL
+//             }
+
+//             return {
+//                 name: place.displayName,
+//                 rating: place.rating,
+//                 status: place.businessStatus,
+//                 types: place.types,
+//                 website: place.websiteURI,
+//                 phone: place.nationalPhoneNumber,
+//                 photoUrl,
+//                 location: {
+//                     lat: place.location.lat(),
+//                     lng: place.location.lng(),
+//                 },
+//                 priceLevel: place.priceLevel,
+//                 priceRange: place.priceRange,
+//                 primaryType: place.primaryType,
+//                 ratingCount: place.userRatingCount,
+//                 primaryTypeDisplayName: place.primaryTypeDisplayName,
+//             };
+//         })
+//     );
+
+//     console.log("getPlacesDataByGoogle ->", formatted);
+
+//     return formatted;
+// };
 
 export const getWeatherData = async (lat, lng) => {
     try {
@@ -54,3 +145,5 @@ export const getWeatherData = async (lat, lng) => {
         console.log(err);
     }
 };
+
+
